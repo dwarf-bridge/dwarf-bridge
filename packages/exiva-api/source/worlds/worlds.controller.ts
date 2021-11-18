03e585cb0cba8614dd9483d2f81282c6a8e967f2
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-18 00:39:57 -0300

    feat: add initial return for findAll controller for worlds

--- packages/exiva-api/source/worlds/worlds.controller.ts
+++ packages/exiva-api/source/worlds/worlds.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { WorldsService } from './worlds.service';
 
 @Controller('worlds')
 export class WorldsController {
   constructor(private readonly worldsService: WorldsService) {}
+
+  @Get()
+  async findAll(): Promise<any> {
+    return [];
+  }
 }
