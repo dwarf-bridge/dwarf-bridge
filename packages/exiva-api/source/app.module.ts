f25417f9e8aad90b7ec53d1680f3f6f3ed7ab8eb
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-17 22:56:38 -0300

    fix: remove referenfces for Character and Guild controllers

--- packages/exiva-api/source/app.module.ts
+++ packages/exiva-api/source/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { WorldsController } from './worlds/worlds.controller';
 import { WorldsService } from './worlds/worlds.service';
-import { GuildsService } from './guilds/guilds.service';
-import { GuildsController } from './guilds/guilds.controller';
-import { CharactersService } from './characters/characters.service';
-import { CharactersController } from './characters/characters.controller';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
