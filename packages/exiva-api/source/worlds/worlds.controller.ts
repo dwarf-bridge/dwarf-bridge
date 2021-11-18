9d0c4f81a26a120dc7464ca8977433a0ddc779b6
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-17 22:53:10 -0300

    fix: case remove unimplemented methods

--- packages/exiva-api/source/worlds/worlds.controller.ts
+++ packages/exiva-api/source/worlds/worlds.controller.ts
@@ -1,18 +1,7 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { WorldsService } from './worlds.service';
+
 @Controller('worlds')
 export class WorldsController {
   constructor(private readonly worldsService: WorldsService) {}
-
-  @Get()
-  async findAll() {
-  }
-
-  @Get(':world')
-  async findOne(@Param('world') world: string) {
-  }
-  @Get('online')
-  findAllOnline(@Query('since') since?: Date): string {
-  @Get('/:world/online')
-  findOnlineByWorld(@Param('world') world: string): string {
-  }
-  }
+}
