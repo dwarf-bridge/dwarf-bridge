99cfb5f4b1619ab324de866423fa521217a9064f
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-26 23:36:43 -0300

    fix(collector): remove unneeded code for game-world services

--- packages/collector/source/game-worlds/game-worlds.service.ts
+++
@@ -1,8 +0,0 @@
-import { Parser } from './parser/parser.service';
-import { Consumer } from './consumer/consumer.service';
-import { Collector } from './collector/collector.service';
-import { Processor } from './processor/processor.service';
-
-export class Service {}
-
-export { Collector, Consumer, Parser, Processor };
