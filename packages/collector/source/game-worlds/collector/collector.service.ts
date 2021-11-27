711f05cf30b81d6deabb41c741b325edccc98510
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-26 23:40:58 -0300

    feat(collector): add initial implementation for collector service

--- packages/collector/source/game-worlds/collector/collector.service.ts
+++ packages/collector/source/game-worlds/collector/collector.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class Collector {
-  async collect() {
+  private readonly logger = new Logger(Collector.name);
+  private page: Observable<AxiosResponse<any, any>>;
+  constructor(private fetch: HttpService, private config: ConfigService) {}
+  public async collect(): Promise<Observable<AxiosResponse<any, any>>> {
     try {
-      /**
-       * get worlds
-       */
-    } catch (err) {}
+      this.logger.log('req | /community/?subtopic=worlds');
+      return this.fetch.get(
+        `${this.config.get('BASE_URL')}${this.config.get('GAME_WORLDS_URL')}`,
+        {
+          headers: {
+            'Accept-Encoding': 'gzip, deflate',
+          },
+        },
+      );
+    } catch (err) {
+      this.logger.error(err);
+    }
   }
-  //   async run(options: { world_name: string }) {}
+
+  public async get_page() {
+    return this.page;
+  }
 }
