81548322b7955b4c04faf9363d5c9b0fd1b4bf0b
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-26 23:40:32 -0300

    feat(collector): add initial implementation of collector's consumer service

--- packages/collector/source/game-worlds/consumer/consumer.service.ts
+++ packages/collector/source/game-worlds/consumer/consumer.service.ts
@@ -1,22 +1,76 @@
 import { Logger } from '@nestjs/common';
 import {
-  Processor,
+  Processor as QueueConsumer,
   Process,
   OnQueueActive,
   OnQueueError,
   OnQueueCompleted,
 } from '@nestjs/bull';
 import { Job } from 'bull';
+import { Collector } from '../collector/collector.service';
+import { Processor } from '../processor/processor.service';
+import { Parser } from '../parser/parser.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GameWorld } from '../world.entity';
+import { ConfigService } from '@nestjs/config';
+// import { CreateWorldDTO } from '../world.dto';
 
-@Processor({
+@QueueConsumer({
   name: 'execution_queue',
 })
 export class Consumer {
-  private readonly logger = new Logger(Consumer.name);
+  private readonly logger = new Logger(`Game Worlds:${Consumer.name}`);
+  constructor(
+    private config: ConfigService,
+    private collector: Collector,
+    private processor: Processor,
+    private parser: Parser,
+    @InjectRepository(GameWorld)
+    private readonly repository: Repository<GameWorld>,
+  ) {}
 
   @Process('GAME_WORLD_CHECK')
   async transcode(job: Job<unknown>) {
+    const schema = this.config.get('TYPEORM_SCHEMA');
     this.logger.log(job.data);
+
+    const response = await this.collector.collect();
+    response.subscribe(async (response) => {
+      const raw_content = this.processor
+        .init(response.data)
+        .handle()
+        .get_content();
+
+      const { regular_worlds, tournament_worlds } = this.parser
+        .init(raw_content)
+        .handle()
+        .get_content();
+
+      const worlds = [
+        ...regular_worlds.map((world) => {
+          return { ...world, is_special_world: false };
+        }),
+        ...tournament_worlds.map((world) => {
+          return { ...world, is_special_world: true };
+        }),
+      ];
+
+      try {
+        await this.repository
+          .createQueryBuilder()
+          .insert()
+          .into(`${schema}.game_worlds`)
+          .values(worlds)
+          .orUpdate(
+            ['server_titles', 'server_conditions', 'creation_date'],
+            ['name'],
+          )
+          .execute();
+      } catch (err) {
+        this.logger.error(err);
+      }
+    });
   }
 
   @OnQueueActive({ name: 'GAME_WORLD_CHECK' })
