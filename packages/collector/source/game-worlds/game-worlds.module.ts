7e806c35530ba94599317f8ce3f919976fb0eec5
Nícolas 'Eowfenth' Deçordi <eowfenth.services@gmail.com>
2021-11-26 23:42:58 -0300

    chore(collector): add game-worlds module

---
+++ packages/collector/source/game-worlds/game-worlds.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { Consumer } from './consumer/consumer.service';
+import { Collector } from './collector/collector.service';
+import { Processor } from './processor/processor.service';
+import { Parser } from './parser/parser.service';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GameWorld } from './world.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature([GameWorld]),
+    HttpModule,
+  ],
+  providers: [Consumer, Collector, Processor, Parser],
+})
+export class GameWorldsModule {}
